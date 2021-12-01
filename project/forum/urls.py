0a7030667b5e6d92989891e81519302feb48563a
Igor Sachinskiy <goga.lon18@gmail.com>
2021-12-01 13:03:46 +0300

    Работающий форум

--- project/forum/urls.py
+++ project/forum/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 
 urlpatterns = [
+    path('<forum_pk>/', views.forum_view, name='forum'),
+    path('<forum_pk>/add/', views.post_add_view, name='post_add'),
+    path('post/<post_pk>/', views.post_view, name='post'),
+    path('post/<post_pk>/add/', views.reply_add_view, name='reply_add'),
     path('', views.index_view, name='index'),
 ]
--- project/forum/views.py
+++ project/forum/views.py
@@ -1,13 +1,55 @@
-from django.http import HttpResponseForbidden
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-from project.slaves.forms import ResumeForms
-from project.slaves.models import Resume
-
+from . import models
+from . import forms
 
 def index_view(request):
     """Вью отображает главную страницу приложения"""
+    qs = models.Group.objects.all()
     context = {
-            'queryset': None,
+            'queryset': qs,
             }
     return render(request, 'forum/index.html', context)
+
+
+def forum_view(request, forum_pk):
+    """Отображаем содержимое указанного форума"""
+    forum = models.Forum.objects.get(pk=forum_pk)
+    context = {
+        'forum': forum,
+    }
+    return render(request, 'forum/forum.html', context)
+
+
+def post_view(request, post_pk):
+    """Отображаем содержимое поста"""
+    post = models.Post.objects.get(pk=post_pk)
+    context = {
+        'post': post,
+    }
+    return render(request, 'forum/post.html', context)
+
+
+@login_required
+def post_add_view(request, forum_pk):
+    form = forms.PostForm(request.POST or None)
+    forum = models.Forum.objects.get(pk=forum_pk)
+    if request.method == 'POST':
+        if form.is_valid():
+            obj = form.save(request.user, forum)
+            return  redirect(obj)
+    context = {'form': form, 'forum': forum, 'about': 'Новый пост'}
+    return render(request, 'forum/form.html', context)
+
+
+@login_required
+def reply_add_view(request, post_pk):
+    form = forms.ReplyForm(request.POST or None)
+    post = models.Post.objects.get(pk=post_pk)
+    if request.method == 'POST':
+        if form.is_valid():
+            obj = form.save(request.user, post)
+            return  redirect(obj)
+    context = {'form': form, 'forum': post.forum, 'post': post, 'about': f'Новый отклик на "{post.text}"'}
+    return render(request, 'forum/form.html', context)
